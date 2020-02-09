438ce37c1de10cc7749964b3897ba9576f5e9817
SYSTEX_brianzheng <brianzheng@systex.com>
2020-02-09 16:09:39 +0800

    httpclient using  get method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'demo';
 
-  keyword = '';
+  keyword = 'demo1';
   // keywordClick = '';
 
+  data: any;
   clearKeyWord() {
+    // console.log(this.keyword);
     this.keyword = '';
   }
 
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('/api/articles.json')
+      .subscribe((value) => {
+        console.log(value);
+        this.data = value;
+      });
+  }
 }
