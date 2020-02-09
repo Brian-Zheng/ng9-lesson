1ddc80866d40a42339861111503fbfcd1d1cecf8
SYSTEX_brianzheng <brianzheng@systex.com>
2020-02-09 13:21:11 +0800

    內嵌繫結  {{val}}

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  sitename = 'The Will Will Web';
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.sitename = 'in ng on init';
+    }, 2000);
   }
-
 }
