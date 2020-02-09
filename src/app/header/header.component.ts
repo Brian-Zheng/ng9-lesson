944db6738083c3eeeb3663a819a4a873d10b59ae
SYSTEX_brianzheng <brianzheng@systex.com>
2020-02-09 14:17:04 +0800

    雙向資料綁定  [(ngModel)]="val"
    keyup.escape練習

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,18 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   sitename = 'The Will Will Web';
-
   imgsrc = '/assets/images/logo.png';
-
   subtitle = '記載著 Will 在網路世界的學習心得與技術分享';
-
   selfprop = '自定義property';
-
   showicons = true;
-
-  changeTitle() {
-    this.sitename = this.sitename + '!';
-  }
 
   constructor() { }
 
@@ -27,5 +19,17 @@ export class HeaderComponent implements OnInit {
     setTimeout(() => {
       this.sitename = 'in ng on init';
     }, 2000);
+  }
+
+  changeTitle(args) {
+    console.log(args);
+    if(args.ctrlKey) {
+      this.sitename = this.sitename + '!';
+    }
+  }
+
+  al(args) {
+    alert('trigger ctrl+C');
+    console.log(args);
   }
 }
